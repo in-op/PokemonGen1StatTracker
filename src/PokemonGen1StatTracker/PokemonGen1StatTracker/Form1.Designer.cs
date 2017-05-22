2b0e435baab038b574ae37f578d80d48af6a3c3b
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-21 20:19:48 -0400

    Created form project and skeleton

--- src/PokemonGen1StatTracker/PokemonGen1StatTracker/Form1.Designer.cs
+++ src/PokemonGen1StatTracker/PokemonGen1StatTracker/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.spriteBox = new System.Windows.Forms.PictureBox();
+            this.pokemonDropDown = new System.Windows.Forms.ComboBox();
+            this.pokemonLabel = new System.Windows.Forms.Label();
+            this.levelLabel = new System.Windows.Forms.Label();
+            this.levelInput = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spriteBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.spriteBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pokemonDropDown, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pokemonLabel, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.levelLabel, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.levelInput, 5, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1182, 753);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // spriteBox
+            // 
+            this.spriteBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.spriteBox, 2);
+            this.spriteBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spriteBox.Location = new System.Drawing.Point(3, 3);
+            this.spriteBox.Name = "spriteBox";
+            this.tableLayoutPanel1.SetRowSpan(this.spriteBox, 2);
+            this.spriteBox.Size = new System.Drawing.Size(230, 182);
+            this.spriteBox.TabIndex = 0;
+            this.spriteBox.TabStop = false;
+            // 
+            // pokemonDropDown
+            // 
+            this.pokemonDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.pokemonDropDown.FormattingEnabled = true;
+            this.pokemonDropDown.Location = new System.Drawing.Point(593, 97);
+            this.pokemonDropDown.Name = "pokemonDropDown";
+            this.pokemonDropDown.Size = new System.Drawing.Size(112, 24);
+            this.pokemonDropDown.TabIndex = 1;
+            // 
+            // pokemonLabel
+            // 
+            this.pokemonLabel.AutoSize = true;
+            this.pokemonLabel.Location = new System.Drawing.Point(475, 94);
+            this.pokemonLabel.Name = "pokemonLabel";
+            this.pokemonLabel.Size = new System.Drawing.Size(71, 17);
+            this.pokemonLabel.TabIndex = 2;
+            this.pokemonLabel.Text = "Pokemon:";
+            // 
+            // levelLabel
+            // 
+            this.levelLabel.AutoSize = true;
+            this.levelLabel.Location = new System.Drawing.Point(475, 0);
+            this.levelLabel.Name = "levelLabel";
+            this.levelLabel.Size = new System.Drawing.Size(46, 17);
+            this.levelLabel.TabIndex = 3;
+            this.levelLabel.Text = "Level:";
+            // 
+            // levelInput
+            // 
+            this.levelInput.Location = new System.Drawing.Point(593, 3);
+            this.levelInput.Name = "levelInput";
+            this.levelInput.Size = new System.Drawing.Size(100, 22);
+            this.levelInput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spriteBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox spriteBox;
+        private System.Windows.Forms.ComboBox pokemonDropDown;
+        private System.Windows.Forms.Label pokemonLabel;
+        private System.Windows.Forms.Label levelLabel;
+        private System.Windows.Forms.TextBox levelInput;
     }
 }
 
