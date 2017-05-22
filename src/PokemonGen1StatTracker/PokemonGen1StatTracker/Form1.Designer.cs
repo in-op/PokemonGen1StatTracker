26756e5a8af8c38d8c444431cb62c02acdd2d3f8
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-05-21 20:43:40 -0400

    Added labels for each stat and text boxes for entering numbers

--- src/PokemonGen1StatTracker/PokemonGen1StatTracker/Form1.Designer.cs
+++ src/PokemonGen1StatTracker/PokemonGen1StatTracker/Form1.Designer.cs
@@ -34,6 +34,16 @@
             this.pokemonLabel = new System.Windows.Forms.Label();
             this.levelLabel = new System.Windows.Forms.Label();
             this.levelInput = new System.Windows.Forms.TextBox();
+            this.hpLabel = new System.Windows.Forms.Label();
+            this.hpTextBox = new System.Windows.Forms.TextBox();
+            this.attackTextBox = new System.Windows.Forms.TextBox();
+            this.defenseTextBox = new System.Windows.Forms.TextBox();
+            this.specialTextBox = new System.Windows.Forms.TextBox();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            this.attackLabel = new System.Windows.Forms.Label();
+            this.defenseLabel = new System.Windows.Forms.Label();
+            this.specialLabel = new System.Windows.Forms.Label();
+            this.speedLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.spriteBox)).BeginInit();
             this.SuspendLayout();
@@ -56,6 +66,16 @@
             this.tableLayoutPanel1.Controls.Add(this.pokemonLabel, 4, 1);
             this.tableLayoutPanel1.Controls.Add(this.levelLabel, 4, 0);
             this.tableLayoutPanel1.Controls.Add(this.levelInput, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.hpTextBox, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.attackTextBox, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.defenseTextBox, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.specialTextBox, 5, 3);
+            this.tableLayoutPanel1.Controls.Add(this.speedTextBox, 6, 3);
+            this.tableLayoutPanel1.Controls.Add(this.hpLabel, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.attackLabel, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.defenseLabel, 4, 2);
+            this.tableLayoutPanel1.Controls.Add(this.specialLabel, 5, 2);
+            this.tableLayoutPanel1.Controls.Add(this.speedLabel, 6, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -117,6 +137,96 @@
             this.levelInput.Size = new System.Drawing.Size(100, 22);
             this.levelInput.TabIndex = 4;
             // 
+            // hpLabel
+            // 
+            this.hpLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.hpLabel.AutoSize = true;
+            this.hpLabel.Location = new System.Drawing.Point(281, 226);
+            this.hpLabel.Name = "hpLabel";
+            this.hpLabel.Size = new System.Drawing.Size(27, 17);
+            this.hpLabel.TabIndex = 5;
+            this.hpLabel.Text = "HP";
+            // 
+            // hpTextBox
+            // 
+            this.hpTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.hpTextBox.Location = new System.Drawing.Point(245, 318);
+            this.hpTextBox.Name = "hpTextBox";
+            this.hpTextBox.Size = new System.Drawing.Size(100, 22);
+            this.hpTextBox.TabIndex = 6;
+            // 
+            // attackTextBox
+            // 
+            this.attackTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.attackTextBox.Location = new System.Drawing.Point(363, 318);
+            this.attackTextBox.Name = "attackTextBox";
+            this.attackTextBox.Size = new System.Drawing.Size(100, 22);
+            this.attackTextBox.TabIndex = 7;
+            // 
+            // defenseTextBox
+            // 
+            this.defenseTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.defenseTextBox.Location = new System.Drawing.Point(481, 318);
+            this.defenseTextBox.Name = "defenseTextBox";
+            this.defenseTextBox.Size = new System.Drawing.Size(100, 22);
+            this.defenseTextBox.TabIndex = 8;
+            // 
+            // specialTextBox
+            // 
+            this.specialTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.specialTextBox.Location = new System.Drawing.Point(599, 318);
+            this.specialTextBox.Name = "specialTextBox";
+            this.specialTextBox.Size = new System.Drawing.Size(100, 22);
+            this.specialTextBox.TabIndex = 9;
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.speedTextBox.Location = new System.Drawing.Point(717, 318);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(100, 22);
+            this.speedTextBox.TabIndex = 10;
+            // 
+            // attackLabel
+            // 
+            this.attackLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.attackLabel.AutoSize = true;
+            this.attackLabel.Location = new System.Drawing.Point(389, 226);
+            this.attackLabel.Name = "attackLabel";
+            this.attackLabel.Size = new System.Drawing.Size(47, 17);
+            this.attackLabel.TabIndex = 11;
+            this.attackLabel.Text = "Attack";
+            // 
+            // defenseLabel
+            // 
+            this.defenseLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.defenseLabel.AutoSize = true;
+            this.defenseLabel.Location = new System.Drawing.Point(500, 226);
+            this.defenseLabel.Name = "defenseLabel";
+            this.defenseLabel.Size = new System.Drawing.Size(61, 17);
+            this.defenseLabel.TabIndex = 12;
+            this.defenseLabel.Text = "Defense";
+            // 
+            // specialLabel
+            // 
+            this.specialLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.specialLabel.AutoSize = true;
+            this.specialLabel.Location = new System.Drawing.Point(622, 226);
+            this.specialLabel.Name = "specialLabel";
+            this.specialLabel.Size = new System.Drawing.Size(54, 17);
+            this.specialLabel.TabIndex = 13;
+            this.specialLabel.Text = "Special";
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(742, 226);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(49, 17);
+            this.speedLabel.TabIndex = 14;
+            this.speedLabel.Text = "Speed";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -140,6 +250,16 @@
         private System.Windows.Forms.Label pokemonLabel;
         private System.Windows.Forms.Label levelLabel;
         private System.Windows.Forms.TextBox levelInput;
+        private System.Windows.Forms.TextBox hpTextBox;
+        private System.Windows.Forms.TextBox attackTextBox;
+        private System.Windows.Forms.TextBox defenseTextBox;
+        private System.Windows.Forms.TextBox specialTextBox;
+        private System.Windows.Forms.TextBox speedTextBox;
+        private System.Windows.Forms.Label hpLabel;
+        private System.Windows.Forms.Label attackLabel;
+        private System.Windows.Forms.Label defenseLabel;
+        private System.Windows.Forms.Label specialLabel;
+        private System.Windows.Forms.Label speedLabel;
     }
 }
 
